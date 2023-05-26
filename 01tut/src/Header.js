a52381f71e7e637b3a3c67074cc100568da0c5e0
Kiril Stoyanov <kirilsstoyanov3@gmail.com>
2023-05-26 22:02:50 +0300

    Groceries List

--- 01tut/src/Header.js
+++ 01tut/src/Header.js
@@ -1,12 +1,10 @@
 const Header = () => {
 
-
-  
-  return (
-    <header>
-      <h1>Groceries List</h1>
-    </header>
-  )
-  }
+    return (
+        <header>
+            <h1>Groceries List</h1>
+        </header>
+    )
+}
 
 export default Header;
